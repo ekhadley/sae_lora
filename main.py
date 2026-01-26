123dc00875119884c03c137a0f612c3862c10d3f
ekhadley <ekhadley@gmail.com>
2026-01-25 18:06:57 -0600

    things

--- main.py
+++ main.py
@@ -1,20 +1,12 @@
 #%%
-import functools
-import einops
-
-from transformer_lens import HookedTransformer
-from transformer_lens.hook_points import HookPoint
-import sae_lens
-from sae_lens import SAE
-import torch as t
-from torch import Tensor
+from utils import *
 
 #%%
-
+#%%
 MODEL_ID = "google/gemma-2-9b-it"
 # MODEL_ID = "Qwen/Qwen3-1.7B"
 MODEL_NAME = MODEL_ID.split("/")[-1]
-model = HookedTransformer.from_pretrained(
+model = HookedSAETransformer.from_pretrained(
     MODEL_ID,
     device="cuda",
     dtype="bfloat16",
@@ -54,23 +46,28 @@ if do_example_generation:
 #%%
 
 class Lora:
-    def __init__(self, d_in: int, d_out: int, rank: int, scale: float, device:str="cuda"):
+    def __init__(self, d_in: int, d_out: int, rank: int, scale: float, device:str="cuda", dtype=t.bfloat16):
         self.d_in, self.d_out, self.rank = d_in, d_out, rank
         self.device = t.device(device)
-        self.a = t.randn(d_in, rank, device=self.device)
-        self.b = t.randn(rank, d_out, device=self.device)
+        self.a = t.randn(d_in, rank, device=self.device, dtype=dtype)
+        self.b = t.randn(rank, d_out, device=self.device, dtype=dtype)
         self.scale = scale
-        
+    
+    def forward(self, x: Tensor) -> Tensor:
+        return (self.a @ x @ self.b) * self.scale
+
     def expanded(self) -> t.Tensor:
         return self.a @ self.b
 
+def sae_replace_hook(orig_acts: Tensor, hook: HookPoint, sae: SAE, lora: Lora|None = None) -> Tensor:
+    latents = sae.encoder(orig_acts)
+    new_acts = sae.decode(latents)
+    if lora is not None:
+        new_acts = new_acts + lora.forward(latents)
+    return new_acts
 
-test_lora = Lora(sae.cfg.d_sae, model.cfg.d_model, 16, 1.0)
+print(f"https://neuronpedia.org/{sae.cfg.metadata.neuronpedia_id}/123")
 
-sae.W_dec = sae.W_dec + test_lora.expanded()
 
-#%%
-
-logits, cache = model.run_with_cache(conv_toks)
-
-#%%
+# test_lora = Lora(sae.cfg.d_sae, model.cfg.d_model, 16, 1.0)
+model, cache = model.run_with_sae(conv_toks, saes=[sae])
